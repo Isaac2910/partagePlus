10a1453fd097aab004fbc32ced894d04e5b9280c
Newton10 <isaacomar29@gmail.com>
2026-07-20 12:46:17 +0100

    v1 serializ

---
+++ api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from .models import User, Categorie, Don, Reservation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id_user', 'username', 'email', 'first_name', 'last_name', 'latitude', 'longitude']
+
+
+
+class CategorieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categorie
+        fields = ['id_categorie', 'nom_categorie', 'description']
+
+
+class DonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Don
+        fields = ['id_don', 'titre', 'description', 'date_debut', 'date_fin', 'id_categorie']
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['id_reservation', 'id_user', 'id_don', 'date_reservation']   
+
+
+class notificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['id_reservation', 'id_user', 'id_don', 'date_reservation'] 
+        
+
+class SignalerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Don
+        fields = ['id_don', 'titre', 'description', 'type_don', 'quantite', 'photo_don', 'latitude', 'longitude', 'address', 'date_publier', 'date_expiration', 'statut', 'message', 'id_user', 'id_categorie']
